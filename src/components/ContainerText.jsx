7eac39346558f2a28164fa1ded49498542123507
Luiz Felipe <lfar05071@gmail.com>
2022-10-31 20:50:10 -0300

    sexto commit

--- src/components/ContainerText.jsx
+++ src/components/ContainerText.jsx
@@ -11,7 +11,8 @@ const ContainerText = () => {
             <h1>Esse texto é uma heading super legal</h1>
             <p>E esse texto aqui acompanha o título. Já que eu preciso falar muito,
               vou falar sobre Emas. Considerada a maior ave do continente americano,
-              a ema pode chegar a medir 1,70 m de altura! Impressionante, não é?</p>         
+              a ema pode chegar a medir 1,70 m de altura! Impressionante, não é?</p>
+              <button>Isso aqui é um CTA</button>       
         </div>    
         <div className='img'>
           <img className='img-not' src={not} />
